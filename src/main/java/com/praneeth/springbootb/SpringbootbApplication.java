aa64c85c8cc93cc0904c0dde05e7cfe0b447c0eb
mpraneeth18 <praneethm31@gmail.com>
2026-06-12 16:46:10 +0530

    These are first implementation

---
+++ src/main/java/com/praneeth/springbootb/SpringbootbApplication.java
@@ -0,0 +1,13 @@
+package com.praneeth.springbootb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praneeth/springbootb/SpringbootbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praneeth.springbootb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
